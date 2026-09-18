1568cc095cf2742750bb51d38cfaeb67bbda4e97
raut-abhishek <iamabhishekraut01@gmail.com>
2026-09-18 23:45:29 +0530

    Add CircuitBreaker state machine unit test

---
+++ ratelimiter/src/test/java/com/demo/CircuitBreakerTest.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+
+public class CircuitBreakerTest {
+
+	@Test
+	public void testCircuitBreakerCycle() throws InterruptedException {
+		CircuitBreaker breaker = new CircuitBreaker(3, 1000);
+
+		assertTrue(breaker.allowRequest());
+		assertTrue(breaker.allowRequest());
+		assertTrue(breaker.allowRequest());
+
+		breaker.recordFailure();
+		breaker.recordFailure();
+		breaker.recordFailure();
+
+		assertFalse(breaker.allowRequest());
+
+		Thread.sleep(1100);
+
+		assertTrue(breaker.allowRequest());
+		breaker.recordSuccess();
+		assertTrue(breaker.allowRequest());
+
+	}
+}
