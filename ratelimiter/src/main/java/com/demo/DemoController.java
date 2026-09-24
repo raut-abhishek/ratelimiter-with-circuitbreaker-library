be148093fa47e790ef5303168e048ac7a8930ad4
raut-abhishek <iamabhishekraut01@gmail.com>
2026-09-24 18:38:10 +0530

    Add DemoApplication and DemoController with rate limiting and exception handling

---
+++ ratelimiter/src/main/java/com/demo/DemoController.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.annotation.RateLimited;
+import com.demo.aspect.RateLimitExceedException;
+
+@RestController
+public class DemoController {
+	
+	@RateLimited(capacity=3, refillRate = 1)
+	@GetMapping("/hello")
+	public String hello() {
+		return "Request allowed!";
+	}
+	
+	
+	
+	@ExceptionHandler(RateLimitExceedException.class)
+	@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+	public String handleRateLimitException(RateLimitExceedException ex) {
+		return ex.getMessage();
+	}
+}
