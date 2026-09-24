40c6a25a9603f146fd2080fc166c091d30b2508a
raut-abhishek <iamabhishekraut01@gmail.com>
2026-09-24 13:00:22 +0530

    Update test imports to use JUnit 5 (new version)assertions

--- ratelimiter/src/test/java/com/demo/AdaptiveTokenBucketTest.java
+++ ratelimiter/src/test/java/com/demo/AdaptiveTokenBucketTest.java
@@ -1,8 +1,8 @@
 package com.demo;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
 
 public class AdaptiveTokenBucketTest {
 
