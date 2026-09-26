e71e331c87b9ffb28561e1e03b935fa8a1e1f556
raut-abhishek <iamabhishekraut01@gmail.com>
2026-09-26 23:25:20 +0530

    Add CircuitBreakerProtected annotation for circuit breaker configuration

---
+++ ratelimiter/src/main/java/com/demo/annotation/CircuitBreakerProtected.java
@@ -0,0 +1,16 @@
+package com.demo.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CircuitBreakerProtected {
+	int threshold() default 5;
+	long cooldownTime() default 3000;
+	
+}
